d2a4d0f02f692b91e14d7b2da3c0a92ed95bd84a
AnercioRibeiro <anercio.ribeiro@outlook.com>
2023-03-11 21:21:10 +0100

    Add interface model to client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Pagination } from './models/pagination';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'Skinet';
-  products: any[] = [];
+  products: Product[] = [];
 
   constructor(private http: HttpClient){ }
-
 
     ngOnInit(): void {
       //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
       //Add 'implements OnInit' to the class.
-      this.http.get('http://localhost:5125/api/product')
+      this.http.get<Pagination<Product[]>>('http://localhost:5125/api/product')
         .subscribe({
           next: (response: any) => this.products = response.data,
           error: error => console.log(error),
