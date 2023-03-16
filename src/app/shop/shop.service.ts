1518209bb459a45de6a084b9d2dd436cd917983d
AnercioRibeiro <anercio.ribeiro@outlook.com>
2023-03-16 11:26:57 +0100

    Http call bug fixed

--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, map, Observable } from 'rxjs';
+import { Brand } from '../shared/models/brand';
+import { Type } from '../shared/models/type';
 import { Pagination } from '../shared/models/pagination';
 import { Product } from '../shared/models/product';
 
@@ -7,11 +10,20 @@ import { Product } from '../shared/models/product';
   providedIn: 'root'
 })
 export class ShopService {
+  brands: Brand[] = [];
   baseUrl = 'http://localhost:5125/api/';
 
   constructor(private http: HttpClient) { }
 
   getProducts(){
-    return this.http.get<Pagination<Product[]>>(this.baseUrl +'product?pageSize=50');
+    return this.http.get<Pagination<Product[]>>(this.baseUrl+'products');
   }
+  getBrands(){
+    return this.http.get<Brand[]>(this.baseUrl+'products/brands');
+  }
+
+  getTypes(){
+    return this.http.get<Type[]>(this.baseUrl+'products/types');
+    }
+
 }
