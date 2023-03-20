b49edbdc0fd48a2b5b5815747bae2d711d3931d1
AnercioRibeiro <anercio.ribeiro@outlook.com>
2023-03-20 12:09:02 +0100

    Add lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http'
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
-import { HomeComponent } from './home/home.component';
 import { HomeModule } from './home/home.module';
 
 
@@ -23,7 +21,6 @@ import { HomeModule } from './home/home.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule,
     HomeModule,
   ],
   providers: [],
